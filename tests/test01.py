2e68b0d566a1a731746489d1d6384ec98ec764fd
ia101 <idan101@gmail.com>
2018-02-24 16:10:25 +0200

    string change

--- tests/test01.py
+++ tests/test01.py
@@ -1,5 +1,5 @@
 name = input("Give me your name: ")
 age  = int((input("Give me your age: ")))
-copies  = int((input("Give me your age: ")))
+copies  = int((input("Give me number of copies: ")))
 diff = 2018+(100-age)
 print(("Hello " + name + ", you will be 100 at: " + str(diff) + "\n") * copies)
