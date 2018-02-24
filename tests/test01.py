9000f8affab3be97c57bd3caae3836c1cdcb4a0d
ia101 <idan101@gmail.com>
2018-02-24 15:32:47 +0200

    python to git

---
+++ tests/test01.py
@@ -0,0 +1,5 @@
+name = input("Give me your name: ")
+age  = int((input("Give me your age: ")))
+copies  = int((input("Give me your age: ")))
+diff = 2018+(100-age)
+print(("Hello " + name + ", you will be 100 at: " + str(diff) + "\n") * copies)
